f1883e1ac0d1cd1a643306231c8f2e3193dc894a
sanggae4133 <sanggae4133@naver.com>
2020-08-05 22:56:41 +0900

    first commit

---
+++ mystorage/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Essay, Album, Files
+from .serializers import EssaySerializer, AlbumSerializer, FileSerializer
+from rest_framework.filters import SearchFilter
+
+class PostViewSet(viewsets.ModelViewSet):
+    
+    queryset = Essay.objects.all()
+    serializer_class = EssaySerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ('tilte', 'body', ) #튜플로 작성
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    #현재 request를 보낸 유저
+    # == self.request.user
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author = self.request.user)
+        else:
+            qs = qs.none()
+
+        return qs
+
+class ImgViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = Files.objects.all()
+    serializer_class = FileSerializer
